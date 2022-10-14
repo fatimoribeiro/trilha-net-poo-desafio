51d7cb022dc2489dacfcedb39974bbb00eaee2cd
Fatimo Ribeiro <fatimoribeiro@gmail.com>
2022-10-14 17:26:31 -0300

    Alterações Feitas

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia("98765-4321", "Nokia N1", "76325659876", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Iphone iphone = new Iphone("96832-9945", "Iphone 12", "94820486299", 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Facebook");
